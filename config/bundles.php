9d0174ed362317907f156b176f007df554fa3df4
Ron <ron.gahler@wolterskluwer.com>
2025-12-24 12:29:49 +0100

    Added Symfony UX with Vue

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,8 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Vue\VueBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
--- src/Game/Game/Player/Entity/Player.php
+++ src/Game/Game/Player/Entity/Player.php
@@ -37,32 +37,4 @@ class Player implements EntityUuidInterface
 
     public function setId(?int $id): self
     {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getUser(): User
-    {
-        return $this->user;
-    }
-
-    public function setUser(User $user): Player
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-}
+        $this->
